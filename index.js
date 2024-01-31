d20a7193aa3e56d60f8b01fb84e6fb266826e6cb
oluyomi justina <justinaolamide2611@gmail.com>
2024-01-31 15:04:32 +0100

    adjust media

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ let bar =document.querySelector('#menu');
 let menu =document.querySelector('.alt-menu');
 
 bar.onclick = () => {
-    bar.classList.add("fa-bars");
     menu.classList.toggle("show");
 };
+ 
